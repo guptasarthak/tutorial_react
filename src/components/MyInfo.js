87d591ea8cebb1aa991f3948b30570330c2d61ae
sarthak gupta <sarthak.gupta@live.com>
2020-09-15 17:15:03 -0400

    creat component folder

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return ( 
+        <div>
+            <h1>Hi! Sarthak</h1>
+            <p>I am learning React JS so that I can build apps and website</p>
+            <ul>
+                <li>Iceland</li>
+                <li>Croatia</li>
+                <li>Brazil</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function MyInfo() {
-    return ( 
-        <div>
-            <h1>Hi! Sarthak</h1>
-            <p>I am learning React JS so that I can build apps and website</p>
-            <ul>
-                <li>Iceland</li>
-                <li>Croatia</li>
-                <li>Brazil</li>
-            </ul>
-        </div>
-    )
-}
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"))
